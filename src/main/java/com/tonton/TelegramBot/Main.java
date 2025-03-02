a68b8853098213544d464945c0e1abb9e7dcfe7d
HrUnDeL-sus <pendosfromrussia@gmail.com>
2025-03-02 08:52:48 +0300

    fix bug

--- src/main/java/com/tonton/TelegramBot/Main.java
+++ src/main/java/com/tonton/TelegramBot/Main.java
@@ -1,6 +1,8 @@
 package com.tonton.TelegramBot;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.GameRule;
@@ -10,6 +12,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,6 +27,8 @@ public class Main extends JavaPlugin implements Listener {
 	TelegramBotsApi botsApi;
 	BotSession botSession;
 	Bot bot=new Bot();
+	List<Player> _joinPlayers=new ArrayList<Player>();
+	List<Player> _leavePlayers=new ArrayList<Player>();
 	 @Override
 	    public void onEnable(){
 		 getServer().getPluginManager().registerEvents(this, this);
@@ -43,14 +48,28 @@ public class Main extends JavaPlugin implements Listener {
 	    		
 	    }
 	    @EventHandler
+	    public void OnMove(PlayerMoveEvent e)
+	    {
+	    	 if (e.getTo().getBlockX() == e.getFrom().getBlockX() && e.getTo().getBlockY() == e.getFrom().getBlockY() && e.getTo().getBlockZ() == e.getFrom().getBlockZ()) return; //The player hasn't moved
+	    	 if(!_joinPlayers.contains(e.getPlayer())) return;
+	    	 _joinPlayers.remove(e.getPlayer());
+	    	 _leavePlayers.add(e.getPlayer());
+	    	 bot.SendNotice("НА СЕРВЕР ЗАШЁЛ ИГРОК:"+e.getPlayer().getName()+"\nТекущий онлайн:"+Bukkit.getOnlinePlayers().size());
+	    }
+	    @EventHandler
 	    public void OnLeave(PlayerQuitEvent e)
 	    {
+	    	if(_joinPlayers.contains(e.getPlayer()))
+	    		_joinPlayers.remove(e.getPlayer());
+	    	if(!_leavePlayers.contains(e.getPlayer())) return;
+	    	_leavePlayers.remove(e.getPlayer());
 	    	bot.SendNotice(e.getPlayer().getName()+" ПОКИНУЛ ЭТОТ БРЕННЫЙ МИР\nТекущий онлайн:"+(Bukkit.getOnlinePlayers().size()-1));
 	    }
 	    @EventHandler
 	    public void OnJoin(PlayerJoinEvent e)
 	    {
-	    	bot.SendNotice("НА СЕРВЕР ЗАШЁЛ ИГРОК:"+e.getPlayer().getName()+"\nТекущий онлайн:"+Bukkit.getOnlinePlayers().size());
+	    	_joinPlayers.add(e.getPlayer());
+	    	
 	    }
 
 }
